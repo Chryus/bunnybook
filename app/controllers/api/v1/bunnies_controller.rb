9afe3aae26a1d870913fe71a63b56539cafcd2dc
Chris <chris.haack@me.com>
2017-03-01 09:46:54 -0500

    build CRUD actions

--- app/controllers/api/v1/bunnies_controller.rb
+++ app/controllers/api/v1/bunnies_controller.rb
@@ -4,6 +4,37 @@ module Api
       def index
         render json: Bunny.includes(:hobbies), include: ['hobbies']
       end
+
+      def show
+        render json: Bunny.find(params[:id])
+      end
+
+      def create
+        bunny = Bunny.new(bunny_params)
+        if bunny.save
+          render json: bunny
+        else
+          render json: {status: 500, err: 'bunny could not be saved.'}
+        end
+      end
+
+      def update
+        bunny = Bunny.find(params[:id])
+        bunny.update(bunny_params)
+        render json: {message: "successfully updated!"}, status: 200
+      end
+
+      def destroy
+        bunny = Bunny.find(params[:id])
+        bunny.destroy
+        render json: {message: "successfully deleted!"}, status: 200
+      end
+
+      private
+
+        def bunny_params
+          params.require(:bunny).permit(:name, :breed, :temperament, :weight, :age, :hobby_ids => [])
+        end
     end
   end
 end
--- app/controllers/api/v1/hobbies_controller.rb
+++ app/controllers/api/v1/hobbies_controller.rb
@@ -4,6 +4,37 @@ module Api
       def index
         render json: Hobby.includes(:bunnies), include: ['bunnies']
       end
+
+      def show
+        render json: Hobby.find(params[:id])
+      end
+
+      def create
+        hobby = Hobby.new(hobby_params)
+        if hobby.save
+          render json: hobby
+        else
+          render json: {status: 500, err: 'hobby could not be saved.'}
+        end
+      end
+
+      def update
+        hobby = Hobby.find(params[:id])
+        hobby.update(hobby_params)
+        render json: {message: "successfully updated!"}, status: 200
+      end
+
+      def destroy
+        hobby = Hobby.find(params[:id])
+        hobby.destroy
+        render json: {message: "successfully deleted!"}, status: 200
+      end
+
+      private
+
+        def hobby_params
+          params.require(:hobby).permit(:name, :bunny_ids => [])
+        end
     end
   end
 end
