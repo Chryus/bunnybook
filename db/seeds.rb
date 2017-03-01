a234e34234109765763205422a2fa362504a8a9d
Chris <chris.haack@me.com>
2017-03-01 09:35:00 -0500

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+c1 = Bunny.create(name: "Donna", breed: "Dwarf Hotot", weight: "fat", temperament: "protective", age: 10)
+c2 = Bunny.create(name: "Jasper", breed: "Polish", weight: "skinny", temperament: "sweet and steady", age: 8)
+c3 = Bunny.create(name: "Bean", breed: "Rex", weight: "large", temperament: "sweet", age: 15)
+c4 = Bunny.create(name: "Hakeem", breed: "Lionhead", weight: "average", temperament: "confused", age: 12)
+c5 = Bunny.create(name: "Norman", breed: "English Lop", weight: "a bit fat", temperament: "loyal and loving", age: 5)
+
+h1 = Hobby.create(name: "binkying")
+h2 = Hobby.create(name: "flopping")
+h3 = Hobby.create(name: "chewing")
+h4 = Hobby.create(name: "digging")
+h5 = Hobby.create(name: "head butting")
+
+BunnyHobby.create(bunny: c1, hobby: h1, proficiency: 5)
+BunnyHobby.create(bunny: c2, hobby: h2, proficiency: 5)
+BunnyHobby.create(bunny: c3, hobby: h3, proficiency: 3)
+BunnyHobby.create(bunny: c5, hobby: h5, proficiency: 4)
+BunnyHobby.create(bunny: c5, hobby: h5, proficiency: 2)
