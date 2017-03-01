63b9a1fb772d6e19461f4311f1570d2ecaf5dc8d
Chris <chris.haack@me.com>
2017-03-01 13:06:25 -0500

    select proficiency with hobbies

--- app/models/bunny.rb
+++ app/models/bunny.rb
@@ -1,5 +1,6 @@
 class Bunny < ApplicationRecord
   has_many :bunny_hobbies
-  has_many :hobbies, through: :bunny_hobbies
+  has_many :hobbies, -> { select("hobbies.*, bunny_hobbies.proficiency") },
+              :through => :bunny_hobbies
   accepts_nested_attributes_for :bunny_hobbies
 end
